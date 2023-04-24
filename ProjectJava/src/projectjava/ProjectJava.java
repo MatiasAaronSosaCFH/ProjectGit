cb273bbd571a2a2ed393e88aa1583efb3a73aaa3
Matias Lugones <matiaslugones@live.com.ar>
2023-04-24 20:38:17 -0300

    Primer commit

--- ProjectJava/src/projectjava/ProjectJava.java
+++ ProjectJava/src/projectjava/ProjectJava.java
@@ -11,11 +11,7 @@ package projectjava;
  */
 public class ProjectJava {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
     }
     
 }
